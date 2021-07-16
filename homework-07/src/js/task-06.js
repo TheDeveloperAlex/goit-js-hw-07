3706a35ed5df0401ab7acc28f69874b74a35312a
TheDeveloperAlex <scorpion422@ukr.net>
2021-07-16 17:59:58 +0300

    Исправленная 6 задача

--- homework-07/src/js/task-06.js
+++ homework-07/src/js/task-06.js
@@ -1,6 +1,13 @@
+const number = +document.querySelector('#validation-input').getAttribute('data-length');
+console.log(number);
 const inputNode = document.querySelector('#validation-input');
 inputNode.addEventListener('change', e => {
-    if (e.target.value.length === 6) {
+    if (e.target.value.length === number) {
         e.target.classList.add('valid');
-    } else e.target.classList.add('invalid')
+        e.target.classList.remove('invalid');
+    } else {
+        e.target.classList.add('invalid');
+        e.target.classList.remove('valid');
+
+    };
 });
---
+++ homework-07/src/js/task-08.js
@@ -0,0 +1,24 @@
+let i = 0;
+
+const randInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const RandomRGB = `rgb(${randInt(0, 256)}, ${randInt(0, 256)}, ${randInt(0, 256)})`;
+
+const inputNode = document.querySelector('input[type="number"]');
+// console.log(inputNode.value);
+const createBtnNode = document.querySelector('button[data-action="render"]');
+createBtnNode.addEventListener('click', e => {
+    createBoxes(+inputNode.value);
+});
+
+
+const clearBtnNode = document.querySelector('button[data-action="destroy"]');
+
+
+const divNode = document.querySelector('#boxes');
+
+
+const createBoxes = (amount) => {
+    divNode.insertAdjacentHTML('beforeend', `<div class="id${i++}"></div>`);
+
+    console.log(amount);
+};
