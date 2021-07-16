a5e93c3793d126b7317f13770a5f17d1f76904d7
TheDeveloperAlex <scorpion422@ukr.net>
2021-07-16 16:32:06 +0300

    6 задач

--- homework-07/src/js/task-06.js
+++ homework-07/src/js/task-06.js
@@ -1,1 +1,6 @@
 const inputNode = document.querySelector('#validation-input');
+inputNode.addEventListener('change', e => {
+    if (e.target.value.length === 6) {
+        e.target.classList.add('valid');
+    } else e.target.classList.add('invalid')
+});
